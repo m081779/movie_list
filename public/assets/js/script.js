c43eac7a0f89c1ffc0c595cb59438e697b2d75b3
m081779 <marcoprincipio@hotmail.com>
2017-12-13 12:50:27 -0500

    small tweaks

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,20 +1,21 @@
 
 //function that captures input from textarea, validates it, 
 //and then posts it to the server.
-function createBurger(burger) {
-	let input = burger || $('#burger').val().trim();
+function createmovie(movie) {
+	console.log('asdfasdfasdf',$('#movie'))
+	let input = movie || $('#movie').val().trim();
 	input = input.charAt(0).toUpperCase() + input.slice(1);
 	let clean = /^[a-zA-Z][a-zA-Z0-9 \-']+$/.test(input);
-	$('#burger').val('');
+	$('#movie').val('');
 	if (clean) {
-		$('#burger').attr('placeholder', '');
-		let burger = {};
-		burger.burger_name = input;
-		$.post('/api/burger', burger).then(function (response) {
+		$('#movie').attr('placeholder', '');
+		let movie = {};
+		movie.movie_name = input;
+		$.post('/api/movie', movie).then(function (response) {
 			location.reload();
 		});
 	} else {
-		$('#burger').attr('placeholder', 'Input may not be empty or contain special characters')
+		$('#movie').attr('placeholder', 'Input may not be empty or contain special characters')
 	}
 }
 
@@ -23,9 +24,9 @@ if (annyang) {
 
 	// property defines command, passes
 	//anything said after 'add' as an argument to 
-	//createBurger function and calls it.
+	//createmovie function and calls it.
 	var commands = {
-		'add *burger': createBurger
+		'add *movie': createmovie
 	};
 
 	// Add our commands to annyang 
@@ -43,20 +44,20 @@ if (annyang) {
 //event listener for submit button
 $('#submit').on('click', function (event) {
 	event.preventDefault();
-	createBurger();
+	createmovie();
 });
 
 //event listener for enter key
-$('#burger').keydown(function (e) {
+$('#movie').keydown(function (e) {
  var key = e.which;
  if(key == 13) // the enter key code
   {
-   createBurger();  
+   createmovie();  
   }
 });
 
-//event listener for devour buttons to execute put request
-$(document).on('click', '.devour', function (event) {
+//event listener for watch buttons to execute put request
+$(document).on('click', '.watch', function (event) {
 	event.preventDefault();
 	let id = $(this).attr('id');
 	console.log(id)
